12ed025bd657e3f1c09312bd9764d489edc3567f
Ettore Di Giacinto <mudler@localai.io>
2024-01-02 19:22:59 +0100

    Add mistral-openorca

--- pkg/utils/shorteners.go
+++ pkg/utils/shorteners.go
@@ -2,8 +2,9 @@ package utils
 
 var (
 	modelShorteners map[string]string = map[string]string{
-		"phi-2": "github://mudler/LocalAI/examples/configurations/phi-2.yaml@master",
-		"llava": "github://mudler/LocalAI/examples/configurations/llava.yaml@master",
+		"phi-2":            "github://mudler/LocalAI/examples/configurations/phi-2.yaml@master",
+		"llava":            "github://mudler/LocalAI/examples/configurations/llava.yaml@master",
+		"mistral-openorca": "github://mudler/LocalAI/examples/configurations/mistral-openorca.yaml@master",
 	}
 )
 
